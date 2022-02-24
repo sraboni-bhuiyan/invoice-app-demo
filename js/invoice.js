1cf3b3706ba4b2597527655582a06a39bfc49e72
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-02-24 13:33:13 +0100

    javascripts added

---
+++ js/invoice.js
@@ -0,0 +1,6 @@
+//buyer info
+const detailsButton = document.getElementById("detail-submit-btn").addEventListener("click", function () {
+  const buyerDetails = document.getElementById("buyer-details-input");
+  document.getElementById("buyer-info").innerText = buyerDetails.value;
+  buyerDetails.value = "";
+});
